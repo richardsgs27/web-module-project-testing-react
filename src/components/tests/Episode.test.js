4ea79522ba79cfc30d641cf3f9d80b1407a115ef
Gigi <gigi@iMacs-iMac.local>
2022-05-09 09:20:43 -0500

    Episode is almost completed

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,26 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 7, 
+    image: null, 
+    name:'', 
+    season: 1, 
+    number: 1, 
+    summary: 'test summary', 
+    runtime:1
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => { 
+    render(<Episode episode={testEpisode}/>)
+});
+
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testEpisode}/>)
+    const summary = screen.queryByText(/test summary/i)
+    expect(summary).toBeInTheDocument()
+    expect(summary).toBeTruthy()
+    expect(summary).toHaveTextContent('test summary')
+});
 
 test("renders default image when image is not defined", () => { });
